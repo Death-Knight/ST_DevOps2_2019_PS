43300930dd7c65f6345c7ac6d8e952eff27945fa
Viktar Tsybulko <Death_Knight@tut.by>
2019-02-06 11:08:52 +0300

    21 is so close =)

--- Topic-1/Task_21_Bonus.ps1
+++ Topic-1/Task_21_Bonus.ps1
@@ -1,5 +1,5 @@
 # ST_DevOps2_2019_Viktar_Tsybulko_2_1
-
+#
 #21 Расшифровка http://www.pythonchallenge.com/pc/def/map.html
 # 
 # K -> M
@@ -8,22 +8,42 @@
 #
 # g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.
 #
-
-# идею расшифровки, признаюсь - подглядел, дальше все свое
+#
+# идею расшифровки, признаюсь - подглядел, дальше cам
 
 cls
 $EncodedSring = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
-
+$result = ""
 #$EncodedSring.insert(0,"!").remove(1,1)
+
+for ($i=0; $i -le $EncodedSring.Length - 1; $i++)
+{
+    if ($EncodedSring[$i] -ne (" " -or "." -or "'" -or "(" -or ")"))
+    {
+    $j = [byte][char]$EncodedSring[$i] + 2
+        if ($j -gt 122)
+        {
+            $j = $j - 122 + 96
+        }
+    #$result = $result + [String][char]([byte][char]$EncodedSring[$i]+2)
+    $result = $result + [String][char]$j
+    }
+    else
+    {
+        $result += $EncodedSring[$i]
+    }
+}
+$result
+
+#[byte][char]'a'
+#[byte][char]'z'
+
+#$b = $EncodedSring.ToCharArray()
+#$b[0] = "Z"
+#$b[0]
+#$EncodedSring = -join $b
 #$EncodedSring
-
-
-$b = $EncodedSring.ToCharArray()
-$b[0] = "Z"
-$b[0]
-$EncodedSring = -join $b
-$EncodedSring
-[string]$b
+#[string]$b
 
 #[byte]$c
 #$c = $b[0]
@@ -32,50 +52,19 @@ $EncodedSring
 #[String]([char]([byte]$b[0] + 2))
 
 #[byte]([char]$EncodedSring[0])
-
-
 #[String]([char]([byte]([char]$EncodedSring[0]) + 2))
-#[String]([char]([byte]([char]$EncodedSring[1]) + 2))
-#[String]([char]([byte]([char]$EncodedSring[2]) + 2))
 
 #[string]$s = "!"
 #$s + ([int]$b[0]).ToString()
 #$s = "!!"
 #$s + ([int]$c).ToString()
 
-
-$result = ""
-#$result = $result + [String]1
-#$result
-#$result = $result + ([String]([char]([byte]([char]$EncodedSring[1]) + 2)))
-#$result
-
-for ($i=0; $i -le $b.Count-1; $i++)
-{
-    if ($b[i] -ne ' ')
-    {
-       # $result = $result + ([String]([char]([byte]([char]$EncodedSring[i] + 2 ))))
-    }
-}
-
-$result
-
-
-#for ($i=0; $i -le ($EncodedSring.Length - 1); $i++)
-#{
-#    if ($EncodedSring[i] -ne " ")
-#    {
-#        $result = $result + ([String]([char]([byte]([char]$EncodedSring[i] + 2 ))))
-#    }
-#}
-#$result
-
-cls
-$bb="xyz"
-$bb
-$cb = $bb.ToCharArray()
-$cb
-$cb[0] = 'a'
-$cb
-$bb = -join $cb
-$bb
+#cls
+#$bb="xyz"
+#$bb
+#$cb = $bb.ToCharArray()
+#$cb
+#$cb[0] = 'a'
+#$cb
+#$bb = -join $cb
+#$bb
