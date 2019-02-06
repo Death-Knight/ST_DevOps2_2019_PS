2dd3c6479247ec77a6eef5a3ec2a45bcd8f14207
Viktar Tsybulko <Death_Knight@tut.by>
2019-02-06 12:25:32 +0300

    21 Done! Wooooohoooo!

--- Topic-1/Task_21_Bonus.ps1
+++ Topic-1/Task_21_Bonus.ps1
@@ -8,36 +8,56 @@
 #
 # g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.
 #
-#
-# идею расшифровки, признаюсь - подглядел, дальше cам
+# алгоритм расшифровки без кода реализации, признаюсь - подглядел, дальше cам
 
 cls
 $EncodedSring = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 $result = ""
-#$EncodedSring.insert(0,"!").remove(1,1)
+$onof = 0
 
 for ($i=0; $i -le $EncodedSring.Length - 1; $i++)
 {
-    if ($EncodedSring[$i] -ne (" " -or "." -or "'" -or "(" -or ")"))
+    $onof = 1
+    switch ($EncodedSring[$i])
     {
-    $j = [byte][char]$EncodedSring[$i] + 2
+    " " {$result += $EncodedSring[$i]
+            $onof = 0
+        }
+    "." {$result += $EncodedSring[$i]
+            $onof = 0
+        }
+    "'" {$result += $EncodedSring[$i]
+            $onof = 0
+        }
+    "(" {$result += $EncodedSring[$i]
+            $onof = 0
+        }
+    ")" {$result += $EncodedSring[$i]
+            $onof = 0
+        }
+    }
+    if ($onof -eq 1)
+    {
+        $j = [byte][char]$EncodedSring[$i] + 2
         if ($j -gt 122)
         {
             $j = $j - 122 + 96
         }
-    #$result = $result + [String][char]([byte][char]$EncodedSring[$i]+2)
-    $result = $result + [String][char]$j
-    }
-    else
-    {
-        $result += $EncodedSring[$i]
+        $result = $result + [String][char]$j
     }
 }
 $result
 
+
+#$EncodedSring.insert(0,"!").remove(1,1)
+# рабочий способ замены буквы в строке
+
+#if ($EncodedSring[$i] -ne (" " -or "." -or "'" -or "(" -or ")"))
+# так почему-то не работает ...
+# поэтому перейдем на оператор switch
+
 #[byte][char]'a'
 #[byte][char]'z'
-
 
 #$b = $EncodedSring.ToCharArray()
 #$b[0] = "Z"
