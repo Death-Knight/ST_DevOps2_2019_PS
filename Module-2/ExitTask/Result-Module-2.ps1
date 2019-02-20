802f6214d830c8149bd48baf954a8c18994aa982
Viktar Tsybulko <death_knight@tut.by>
2019-02-20 23:03:58 +0300

    On Time 22:10

---
+++ Module-2/ExitTask/Result-Module-2.ps1
@@ -0,0 +1,66 @@
+﻿# 1
+
+$err = Get-Eventlog -Log "System" -Entrytype "Error" | `
+    Where-Object {$_.TimeGenerated -gt ((Get-Date).AddDays(-7))}
+$err | Export-Clixml -Path "C:\Test\Last_7days_Errors.xml" 
+
+
+# 2
+
+Get-ChildItem -Path "C:\Windows" -Filter *.log | Sort-Object -Descending Name `
+    | Select-Object FullName, Length, CreationTime | Format-Table
+
+
+# 3
+
+.\TwoNumbers.ps1 
+
+
+# 4
+
+$myPassport = Get-Credential Administrator
+
+#$Ser = "ALG" #stop
+$Ser = "BFE" #run
+
+$vms = Get-Content -Path C:\ExitTask2\vms.txt
+$myScript = {    
+    $vmn = (Get-WmiObject Win32_OperatingSystem).CSName
+    $se = Get-Service "ALG"
+    #$se = Get-Service "BFE"
+    if ($se.Status -eq "Running") {    
+        Write-Host -f "green" $vmn, $se.Name -Separator "-"
+    }
+    else {
+        Write-Host -f "red"  $vmn, $se.Name -Separator "-"
+    }
+}
+
+foreach ($vm in $vms) {
+    Invoke-Command -ComputerName $vm -ScriptBlock $myScript -Credential $myPassport
+}
+
+
+# 5
+
+$sourDir = "C:\ExitTask2"
+$destDir = "C:\BackUp"
+$myZIP = "myArchive.zip"
+if (Test-Path -Path $sourDir){
+    Compress-Archive -Path "$sourDir\*" -DestinationPath "$destDir\$myZIP" -Force
+    Write-Output "Done!"
+}
+else {
+    Write-Output "Nothing to compress. Folder $sourDir not exist!"
+}
+
+try {
+$extrDir = "C:\extract"
+    Expand-Archive -Path "$destDir\$myZIP" -DestinationPath $extrDir -Force
+    Write-Output "Done!"
+}
+catch {
+    Write-Output "Can't create output folder!"
+    Break
+}
+
---
+++ Module-2/ExitTask/TwoNumbers.ps1
@@ -0,0 +1,35 @@
+﻿[CmdletBinding()]
+# Parameter help description
+
+Param (    
+    [int]$A_start = 0,
+    [Parameter(Mandatory=$true)]
+    [int]$B_end
+)
+
+if ($A_start -eq $B_end) {
+    if ($A_start%3 -eq 0) {
+        Write-Output "$A_start - bingo!"
+    }
+    else {
+        Write-Output "$A_start = $B_end , sorry."
+    }
+}
+else {
+    if ($A_start -lt $B_end){
+        $internal_start = $A_start
+        $internal_end = $B_end
+        Write-Output "$internal_start $internal_end"
+    }
+    else {
+        $internal_start = $B_end
+        $internal_end = $A_start
+        Write-Output "$internal_start $internal_end"
+    }
+    While ($internal_start -lt $internal_end) {
+    if ($internal_start%3 -eq 0) {
+        Write-Output "Bingo! ^_^ - $internal_start"
+    }      
+    $internal_start++
+    }    
+}
